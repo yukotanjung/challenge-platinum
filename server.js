e4242acf49505faae47bf249f39fd2e7760aa783
Hari <hari@Haris-MacBook-Pro-2.local>
2023-03-29 22:44:36 +0700

    Adding unit testing for get all items, get a item, create new item

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const swaggerDocument = require('./swagger.json');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use("/", router);
 
 
-app.listen(port,() =>{
+const server = app.listen(port,() =>{
     console.log('running server with port ' + port);
 })
+
+module.exports = server
