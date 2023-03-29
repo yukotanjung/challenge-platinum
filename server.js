7e6a90d74ba0cc835d51c87db5cc69176c6ce458
Muchammad Thohari Akbar <thohari.akbar@gmail.com>
2023-03-29 23:31:33 +0700

    Revert "Adding unit testing for get all items, get a item, create new item"

--- server.js
+++ server.js
@@ -7,12 +7,9 @@ const swaggerDocument = require('./swagger.json');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 app.use("/", router);
 app.get('/tes', (req,res)=> res.send('ini sekedar test guys'))
 
-const server = app.listen(port,() =>{
+app.listen(port,() =>{
     console.log('running server with port ' + port);
 })
-
-module.exports = server
